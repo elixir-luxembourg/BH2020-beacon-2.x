0258a614c4712778b0a8582c4ef6716aa011253c
Jacek Lebioda <jacek.lebioda@uni.lu>
2020-11-11 15:35:43 +0100

    chore: corrected schema name

--- deploy/omopdb/omop_query.sql
+++ deploy/omopdb/omop_query.sql
@@ -7,17 +7,17 @@ select distinct on (p.person_id)
 	dis.diseases AS diseases,
 	med.medications as medications,
 	c_rac.concept_name as race
-from "5_3_1".person as p inner join "5_3_1".condition_occurrence as c on p.person_id = c.person_id
-	inner join "5_3_1".concept as c_sex on p.gender_concept_id = c_sex.concept_id
-	inner join "5_3_1".concept as c_eth on p.ethnicity_concept_id = c_eth.concept_id
-	/*inner join "5_3_1".concept as c_dis on c.condition_concept_id = c_dis.concept_id*/
-	inner join "5_3_1".concept as c_rac on p.race_concept_id = c_rac.concept_id
-	inner join "5_3_1".location as loc on p.location_id = loc.location_id
+from "s5_3_1".person as p inner join "s5_3_1".condition_occurrence as c on p.person_id = c.person_id
+	inner join "s5_3_1".concept as c_sex on p.gender_concept_id = c_sex.concept_id
+	inner join "s5_3_1".concept as c_eth on p.ethnicity_concept_id = c_eth.concept_id
+	/*inner join "s5_3_1".concept as c_dis on c.condition_concept_id = c_dis.concept_id*/
+	inner join "s5_3_1".concept as c_rac on p.race_concept_id = c_rac.concept_id
+	inner join "s5_3_1".location as loc on p.location_id = loc.location_id
 	left join lateral (
 		select
 			d.person_id,
 			array_agg(concept_name) as diseases
-		from "5_3_1".condition_occurrence d join "5_3_1".concept c on d.condition_concept_id = c.concept_id
+		from "s5_3_1".condition_occurrence d join "s5_3_1".concept c on d.condition_concept_id = c.concept_id
 		where person_id = p.person_id
 		group by d.person_id
 	) as dis ON TRUE
@@ -25,7 +25,7 @@ from "5_3_1".person as p inner join "5_3_1".condition_occurrence as c on p.perso
 		select
 			d.person_id,
 			array_agg(concept_name) as medications
-		from "5_3_1".drug_exposure d join "5_3_1".concept c on d.drug_concept_id = c.concept_id
+		from "s5_3_1".drug_exposure d join "s5_3_1".concept c on d.drug_concept_id = c.concept_id
 		where person_id = p.person_id
 		group by d.person_id
 	) as med ON TRUE
